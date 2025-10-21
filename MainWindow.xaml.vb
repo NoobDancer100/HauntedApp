d35bbe5f986759620b1f5a9db90d0b0c977bbe5e
NoobDancer100 <perrinhillam@gmail.com>
2025-10-21 08:22:11 -0600

    Add project files.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+Imports System.DirectoryServices.AccountManagement
+Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim FullName = UserPrincipal.Current.DisplayName
+
+        SpookyText.Text = "I see you, " & FullName.Split()(0)
+    End Sub
+End Class
